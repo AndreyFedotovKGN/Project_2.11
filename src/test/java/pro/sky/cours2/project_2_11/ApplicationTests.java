b79435d6c81b1695e021ae4a168d79e77fb1f350
Andrey Fedotov <dj_slider_90@mail.ru>
2022-10-29 18:56:56 +0500

    первый коммит

---
+++ src/test/java/pro/sky/cours2/project_2_11/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.cours2.project_2_11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
